127c3cf4ba09d73b3ae4d6cd87f60f6c4820ffdc
Bhagya5467 <ebdivyanjalee@gmail.com>
2024-09-29 22:52:10 +0530

    Navigation bar links are created.

--- src/components/navigation_bar/index.tsx
+++ src/components/navigation_bar/index.tsx
@@ -3,7 +3,26 @@ import Link from 'next/link';
 function NavigationBar() {
   return (
     <div>
-      NavBar
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/cources">Cources</Link>
+        </li>
+        <li>
+          <Link href="/apply-job">Apply Jobs</Link>
+        </li>
+        <li>
+          <Link href="/resources">Resources</Link>
+        </li>
+        <li>
+          <Link href="/login">Login</Link>
+        </li>
+        <li>
+          <Link href="/sign-up">Sign up</Link>
+        </li>
+      </ul>
     </div>
   );
 }
