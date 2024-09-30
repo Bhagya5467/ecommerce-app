57a4675eb693645f8d758ca6f5f431b578cbd6e8
Bhagya5467 <ebdivyanjalee@gmail.com>
2024-09-30 10:55:55 +0530

    Navbar is created.

--- src/components/navigation_bar/index.tsx
+++ src/components/navigation_bar/index.tsx
@@ -1,30 +1,85 @@
-import Link from 'next/link';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+} from '@nextui-org/react';
 
-function NavigationBar() {
+export default function NavigationBar() {
+
   return (
-    <div>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/cources">Cources</Link>
-        </li>
-        <li>
-          <Link href="/apply-job">Apply Jobs</Link>
-        </li>
-        <li>
-          <Link href="/resources">Resources</Link>
-        </li>
-        <li>
-          <Link href="/login">Login</Link>
-        </li>
-        <li>
-          <Link href="/sign-up">Sign up</Link>
-        </li>
-      </ul>
-    </div>
+    <Navbar>
+      <NavbarBrand>
+        <p className="font-bold text-inherit">Logo</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <Dropdown>
+          <NavbarItem>
+            <DropdownTrigger>
+              <Button
+                disableRipple
+                className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+                radius="sm"
+                variant="light"
+              >
+                FREE TOOLS
+              </Button>
+            </DropdownTrigger>
+          </NavbarItem>
+          <DropdownMenu
+            aria-label="ACME features"
+            className="w-[340px]"
+            itemClasses={{
+              base: 'gap-4',
+            }}
+          >
+            <DropdownItem
+              key="autoscaling"
+              description="ACME scales apps to meet user demand, automagically, based on load."
+            >
+              FREE QUIZZES
+            </DropdownItem>
+            <DropdownItem
+              key="usage_metrics"
+              description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
+            >
+              UB GUIDE
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            COURCES
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            APPLY JOBS
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            RESOURCES
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">LOG IN</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            SIGN UP
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
   );
 }
 
-export default NavigationBar;
